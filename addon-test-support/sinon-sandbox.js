380644854811b6a9e243adf98125f30423fc13f2
Steve Calvert <steve.calvert@gmail.com>
2019-02-16 10:14:30 -0800

    Reducing the size of the public API. Adding clock restore functionality (#101)
    This major change reduces the size of the public API.
    
    1. The disabling of global sinon is removed
    1. The disabling of sinon's global APIs is removed
    
    It also adds additional functionality to include
    
    1. Sinon `useFakeTimers` clock restoration for each test

--- addon-test-support/sinon-sandbox.js
+++ addon-test-support/sinon-sandbox.js
@@ -1,84 +1,68 @@
 import QUnit from 'qunit';
-import { warn } from '@ember/debug';
 
 const SINON = self.sinon;
-const REQUIRED_SANDBOX_APIS = ['clock'];
 
-let errorOnGlobalSinonAccess = false;
+let clockToRestore;
 
-export function setOptions(options = {}) {
-  errorOnGlobalSinonAccess = !!options.errorOnGlobalSinonAccess;
-}
-
+/**
+ * Creates a `sandbox` propery on the test context for access within each test.
+ * Additionally ensures fake timers that are created during a single test are
+ * also restored.
+ *
+ * @public
+ */
 export function createSandbox() {
-  let create = typeof SINON.createSandbox === 'function' ? SINON.createSandbox : SINON.sandbox.create;
-  let sandbox = QUnit.config.current.testEnvironment.sandbox = create();
+  let create =
+    typeof SINON.createSandbox === 'function'
+      ? SINON.createSandbox
+      : SINON.sandbox.create;
+  let sandbox = (QUnit.config.current.testEnvironment.sandbox = create());
 
-  if (errorOnGlobalSinonAccess) {
-    disableSinonGlobal();
-  } else {
-    disableSinonGlobalAPIs(sandbox);
-  }
+  patchUseFakeTimers(sandbox);
 
   sandbox.__restore = sandbox.restore;
 }
 
+/**
+ * Restores each sandbox at the end of each test.
+ *
+ * @public
+ */
 export function restoreSandbox() {
-  QUnit.config.current.testEnvironment.sandbox.__restore();
-  QUnit.config.current.testEnvironment.sandbox = null;
+  let current = QUnit.config.current;
 
-  if (!errorOnGlobalSinonAccess) {
-    Object.defineProperty(self, 'sinon', { value: null });
-  }
-}
-
-function disableSinonGlobal() {
-  Object.defineProperty(self, 'sinon', {
-    get() {
-      throw new Error('Sinon is not available globally because it has been disabled by setting `disableGlobalSinon` to `true` when setting up ember-sinon-sandbox. https://git.io/v56JW')
-    }
-  });
-}
-
-function disableSinonGlobalAPIs(sandbox) {
-  for (let key in SINON) {
-
-    if (!sandbox[key] && REQUIRED_SANDBOX_APIS.indexOf(key) === -1) {
-      if (key === 'createSandbox') {
-        sandbox.createSandbox = function() {
-          warnOnUsage('sinon.createSandbox');
-
-          return sandbox;
-        }
-      } else if (key === 'sandbox') {
-        sandbox.sandbox = {
-          create() {
-            warnOnUsage('sinon.sandbox.create');
-
-            return sandbox;
-          }
-        }
-      } else {
-        Object.defineProperty(sandbox, key, {
-          get() {
-            throw new Error(`The sinon.${key} API is not available when using ember-sinon-sandbox.`);
-          }
-        })
-      }
-    }
+  if (clockToRestore) {
+    clockToRestore.restore();
+    clockToRestore = null;
   }
 
-  Object.defineProperty(self, 'sinon', { value: sandbox });
-
-  return sandbox;
+  current.testEnvironment.sandbox.__restore();
+  current.testEnvironment.sandbox = null;
 }
 
-function warnOnUsage(method) {
-  warn(
-    `Explicitly calling '${method}()' in conjunction with ember-sinon-sandbox is not recommended. Please use 'this.sandbox' available in your tests to access sinon.`,
-    true,
-    {
-      id: 'ember-sinon-sandbox'
+/**
+ * Patches the `useFakeTimers` method in `sinon` to ensure
+ * created clocks are tracked, and subsequently restored after
+ * each test is complete. Calling this more than once within a single
+ * test will result in an error thrown.
+ *
+ * @param {object} sandbox
+ * @returns {object} a clock object returned from `useFakeTimers`
+ */
+function patchUseFakeTimers(sandbox) {
+  let originalUseFakeTimers = sandbox.useFakeTimers;
+
+  sandbox.useFakeTimers = function(...args) {
+    if (clockToRestore) {
+      throw new Error(
+        "You called sinon's useFakeTimers multiple times within the same test. This can result in unknown behavior."
+      );
     }
-  );
+
+    let clock = originalUseFakeTimers(...args);
+
+    clockToRestore = clock;
+
+    return clock;
+  };
 }
--- tests/unit/sinon-sandbox-test.js
+++ tests/unit/sinon-sandbox-test.js
@@ -2,93 +2,100 @@ import { module, test } from 'qunit';
 import {
   createSandbox,
   restoreSandbox,
-  setOptions
 } from 'ember-sinon-sandbox/test-support/sinon-sandbox';
 
-[
-  true,
-  false
-].forEach((errorOnGlobalSinonAccess) => {
-  module(`Unit | ember-sinon-sandbox | With errorOnGlobalSinonAccess ${errorOnGlobalSinonAccess}`, function(hooks) {
-    hooks.before(function() {
-      setOptions({ errorOnGlobalSinonAccess });
-    });
+let previousSandbox;
 
-    test('calling `sinon.sandbox.restore()` can be called explicitly and via `restoreSandbox`', function(assert) {
-      assert.expect(2);
+module(`Unit | ember-sinon-sandbox`, function() {
+  test('calling `sinon.sandbox.restore()` can be called explicitly and via `restoreSandbox`', function(assert) {
+    assert.expect(2);
 
-      const foo = () => true;
-      const bar = { foo };
+    const foo = () => true;
+    const bar = { foo };
 
+    createSandbox();
+
+    this.sandbox.stub(bar, 'foo');
+
+    assert.notEqual(bar.foo, foo);
+
+    this.sandbox.restore();
+
+    assert.equal(bar.foo, foo);
+
+    restoreSandbox();
+  });
+
+  test('using useFakeTimers API continues to work', function(assert) {
+    assert.expect(1);
+
+    createSandbox();
+
+    let clock = this.sandbox.useFakeTimers();
+
+    assert.ok(
+      clock,
+      'The clock API continues to work after forced sandboxing.'
+    );
+
+    restoreSandbox();
+  });
+
+  test('using sinon.useFakeTimers correctly restores clocks', function(assert) {
+    assert.expect(2);
+
+    createSandbox();
+
+    let clock = this.sandbox.useFakeTimers();
+
+    clock.tick(10000);
+
+    assert.equal(Date.now(), 10000);
+
+    restoreSandbox();
+
+    assert.notEqual(Date.now(), 10000);
+  });
+
+  test('using sinon.useFakeTimers multiple times in a single test throws', function(assert) {
+    assert.expect(1);
+
+    createSandbox();
+
+    this.sandbox.useFakeTimers();
+
+    assert.throws(() => {
+      this.sandbox.useFakeTimers();
+    }, /You called sinon's useFakeTimers multiple times within the same test\. This can result in unknown behavior\./);
+
+    restoreSandbox();
+  });
+
+  test('ensures sandbox is restored correctly', function(assert) {
+    assert.expect(1);
+
+    createSandbox();
+    restoreSandbox();
+
+    assert.equal(
+      this.sandbox,
+      null,
+      'Sandbox is set to null after restoration'
+    );
+  });
+
+  test('ensures sandbox instances are different for each test', function(assert) {
+    assert.expect(10);
+
+    for (let i = 0; i < 10; i++) {
       createSandbox();
-
-      this.sandbox.stub(bar, 'foo');
-
-      assert.notEqual(bar.foo, foo);
-
-      this.sandbox.restore();
-
-      assert.equal(bar.foo, foo);
-
+      assert.notEqual(
+        previousSandbox,
+        this.sandbox,
+        'Sandbox instance is unique per test'
+      );
+      previousSandbox = this.sandbox;
       restoreSandbox();
-    });
-
-    test('using useFakeTimers API continues to work', function(assert) {
-      assert.expect(1);
-
-      createSandbox();
-
-      let clock = this.sandbox.useFakeTimers();
-
-      assert.ok(clock, 'The clock API continues to work after forced sandboxing.');
-
-      restoreSandbox();
-    });
-
-    test('using useFakeXMLHttpRequest API continues to work', function(assert) {
-      assert.expect(1);
-
-      let requests = [];
-
-      createSandbox();
-
-      let fakeXHR = this.sandbox.useFakeXMLHttpRequest();
-      fakeXHR.onCreate = (req) => {
-        requests.push(req);
-      };
-
-      let xhr = new XMLHttpRequest();
-      xhr.addEventListener("load", () => {});
-      xhr.open("GET", "http://www.example.org/example.txt");
-      xhr.send();
-
-      assert.equal(requests.length, 1, 'The fake XHR API continues to work after forced sandboxing.');
-
-      restoreSandbox();
-    });
-
-    test('using sinon.assert.* methods throws an error', function(assert) {
-      assert.expect(1);
-
-      createSandbox();
-
-      assert.throws(() => {
-        this.sandbox.assert.calledOnce()
-      }, 'sinon.assert methods throw an error');
-
-      restoreSandbox();
-    });
-
-    test('using sinon.fakeServer.create throws an error', function(assert) {
-      assert.expect(1);
-
-      createSandbox();
-
-      assert.throws(() => {
-        this.sandbox.fakeServer.create()
-      }, 'sandbox.fakeServer.create throws and error');
-
-      restoreSandbox();
-    });
+    }
   });
 });
